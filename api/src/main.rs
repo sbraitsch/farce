8bbe5d7076ab98e232f6e3dad69861364af14455
s-braitsch <simon.braitsch@novatec-gbmh.de>
2025-01-09 17:42:12 +0100

    wasm sandbox

---
+++ api/src/main.rs
@@ -0,0 +1,121 @@
+use std::{
+    fs::OpenOptions,
+    io::Write,
+    process::Command,
+    sync::{Arc, RwLock},
+};
+
+use anyhow::anyhow;
+use axum::{response::Json as JsonResponse, routing::post, Json, Router};
+use serde::{Deserialize, Serialize};
+use wasi_common::{pipe::WritePipe, sync::WasiCtxBuilder};
+use wasmtime::{Engine, Linker, Memory, Module, Store};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/execute", post(execute_code));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Server running on port 3000");
+    axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Deserialize)]
+struct CodeSubmission {
+    source_code: String,
+}
+
+#[derive(Serialize)]
+struct ExecutionResult {
+    stdout: String,
+    result: String,
+}
+
+async fn execute_code(Json(payload): Json<CodeSubmission>) -> JsonResponse<ExecutionResult> {
+    match compile_and_run_wasm(&payload.source_code).await {
+        Ok(result) => JsonResponse(result),
+        Err(err) => JsonResponse(ExecutionResult {
+            stdout: format!("Error: {}", err),
+            result: String::new(),
+        }),
+    }
+}
+
+async fn compile_and_run_wasm(
+    source_code: &str) -> Result<ExecutionResult, anyhow::Error> {
+    let _file = write_file(source_code)?;
+    let output = Command::new("cargo")
+            .args(["build", "--release", "--target", "wasm32-wasip1"])
+            .current_dir("./wasm")
+            .output()?;
+
+    if !output.status.success() {
+        return Err(anyhow!(
+            "Compilation failed: {}",
+            String::from_utf8_lossy(&output.stderr)
+        ));
+    }
+    run_wasm()
+}
+
+fn write_file(source_code: &str) -> Result<(), anyhow::Error> {
+    let mut file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open("./wasm/src/submission.rs")?;
+
+    file.write_all(source_code.as_bytes())?;
+
+    Ok(())
+}
+
+fn run_wasm() -> Result<ExecutionResult, anyhow::Error> {
+    let engine = Engine::default();
+    let mut linker = Linker::new(&engine);
+    // include necessary imports
+    wasi_common::sync::add_to_linker(&mut linker, |s| s)?;
+
+    let stdout_buffer = Arc::new(RwLock::new(Vec::new()));
+    let stdout_pipe = WritePipe::from_shared(stdout_buffer.clone());
+
+    let wasi = WasiCtxBuilder::new()
+        .inherit_stdio()
+        .stdout(Box::new(stdout_pipe))
+        .inherit_args()?
+        .build();
+
+    let mut store = Store::new(&engine, wasi);
+
+    let module = Module::from_file(&engine, "target/wasm32-wasip1/release/wasm.wasm")?;
+    let instance = linker.instantiate(&mut store, &module)?;
+
+    let memory: Memory = instance.get_memory(&mut store, "memory").unwrap();
+
+    let run = instance.get_typed_func::<(), i32>(&mut store, "run")?;
+
+    let ptr = run.call(&mut store, ())? as usize;
+
+    let read = stdout_buffer.read().unwrap();
+    let output = String::from_utf8_lossy(&read);
+
+    let comp_res = resolve_string(&memory.data(&store), ptr)?;
+
+    Ok(ExecutionResult {
+        stdout: output.to_string(),
+        result: comp_res,
+    })
+}
+
+fn resolve_string(memory: &[u8], ptr: usize) -> Result<String, anyhow::Error> {
+    let s = &memory[ptr..ptr + 8];
+    let (p, l) = s.split_at(4);
+    let (str_ptr, length) = (
+        i32::from_ne_bytes(p.try_into()?) as usize,
+        i32::from_ne_bytes(l.try_into()?) as usize,
+    );
+    let string_bytes = &memory[str_ptr..str_ptr + length];
+
+    Ok(String::from_utf8(string_bytes.to_vec())?)
+}
---
+++ wasm/src/lib.rs
@@ -0,0 +1,13 @@
+mod submission;
+
+use submission::execute;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn run() -> *const i32 {
+    let message = serde_json::to_string(&execute()).unwrap();
+    let ptr = message.as_ptr() as i32;
+    let length = message.len() as i32;
+    std::mem::forget(message);
+    let res = Box::new([ptr, length]);
+    Box::into_raw(res) as *const i32
+}
